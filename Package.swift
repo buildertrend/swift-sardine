d72c02cffbe700f2f55262b33607edc757405e41
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2023-07-20 08:02:57 -0500

    Fixed binary url to point to fork in Package.swift.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileIntelligence",
-            url: "https://github.com/oliveratkinson-bc/swift-sardine/releases/download/v1.0.24/MobileIntelligence.xcframework.zip",
+            url: "https://github.com/rzulkoski/swift-sardine/releases/download/v1.0.24/MobileIntelligence.xcframework.zip",
             checksum: "46990a5e8ec7359ff0635ae1dcdcc81e81e512ce02743446ed1877b85634bb51"
         )
     ]

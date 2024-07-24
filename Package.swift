dd80150901f2075bb23858947bac65890f27f18f
Brad P <BradPatras@users.noreply.github.com>
2024-07-24 09:10:18 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileIntelligence",
-            url: "https://github.com/BradPatras/swift-sardine/releases/download/v1.0.34.1/MobileIntelligence.xcframework.zip",
+            url: "https://github.com/BradPatras/swift-sardine/releases/download/v1.0.34-rc/MobileIntelligence.xcframework.zip",
             checksum: "4396edb20b04210b95f2541f8c35d73652052463d14e71bcf91c314245711f36"
         )
     ]
